11a3aa7bd3305ecbe282f8ec4dff9dc24f503851
CAIO MUNIZ IHA <caio.iha@e239.local>
2026-02-25 11:47:07 -0300

    site de Dark Souls

---
+++ atividade-01/app.py
@@ -0,0 +1,28 @@
+# comentario em python
+# importando o flask na aplicação
+from flask import Flask, render_template #render_template renderisa as paginas html
+#carregando o flask em uma variavel
+app = Flask(__name__, template_folder='views') 
+#__name__ é uma variavel de ambiemte do python que tem onome do modulo atual
+
+#criando a rota principal do site
+@app.route('/')
+#def serve para criar uma fumção do no python
+def home():
+    return render_template('index.html')
+
+@app.route('/formulario')
+def formulario():
+    return render_template('formulario.html')
+
+@app.route('/boss')
+def boss():
+    return render_template('boss.html') 
+#iniciando o servidor web
+if __name__ == '__main__': app.run(debug=True)
+#verificando se app.py for o arquivo principal ele inicia o sevidor
+#declarando uma variavel
+
+
+
+
